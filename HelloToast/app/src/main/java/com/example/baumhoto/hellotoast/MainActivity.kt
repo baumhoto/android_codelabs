640b47d51f29d5fa33c0131cb9cb0d95e96ce954
baumhoto <baumhoto@gmail.com>
2018-09-30 08:02:03 +0200

    added landscape and tablet ui for HelloToast

--- HelloToast/app/src/main/java/com/example/baumhoto/hellotoast/MainActivity.kt
+++ HelloToast/app/src/main/java/com/example/baumhoto/hellotoast/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.baumhoto.hellotoast
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var count: Int = 0
+    private var showCount: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.showCount = findViewById(R.id.show_count)
+    }
+
+    fun showToast(view : View) {
+        val toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun countUp(view : View) {
+        this.count++
+        this.showCount?.text = count.toString()
     }
 }
