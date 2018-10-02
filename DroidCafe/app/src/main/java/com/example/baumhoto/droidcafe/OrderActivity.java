138dbb36ed0dae4dcd786763e5624674885a41f7
baumhoto <baumhoto@gmail.com>
2018-10-02 14:21:30 +0200

    added radiobuttons to order dialog

--- DroidCafe/app/src/main/java/com/example/baumhoto/droidcafe/OrderActivity.java
+++ DroidCafe/app/src/main/java/com/example/baumhoto/droidcafe/OrderActivity.java
@@ -1,7 +1,11 @@
 package com.example.baumhoto.droidcafe;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // is the red button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // check which radio buttion was clicked
+        switch (view.getId()) {
+            case R.id.sameday:
+                if(checked)
+                    // same day delivery
+                    displayToast(getString(R.string.same_day_messenger));
+                break;
+            case R.id.nextday:
+                if(checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if(checked)
+                    // pick up
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // do nothing
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
